8b07f9b505e7655822b5e157354e38f6fe2bed84
Luis  Senior <heybluguy@yahoo.com>
2019-06-07 20:04:55 -0500

    get set

--- stage4/Video3/Pet.js
+++ stage4/Video3/Pet.js
@@ -5,38 +5,55 @@ class Pet {
     this.breed = breed;
     this.sound = sound;
   }
-  
+
   get activity() {
     const today = new Date();
     const hour = today.getHours();
-  
+
     if (hour > 8 && hour <= 20) {
       return 'playing';
     } else {
       return 'sleeping';
     }
   }
-  
+
   get owner() {
     return this._owner;
   }
-  
+
   set owner(owner) {
     this._owner = owner;
     console.log(`setter called: ${owner}`);
   }
-  
-  
+
+
   speak() {
     console.log(this.sound);
   }
-} 
+}
+
+class Owner {
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+  }
+
+  get phone() {
+    return this._phone;
+  }
+
+  set phone(phone) {
+    const phoneNormalized = phone.replace(/[^0-9]/g, '');
+    this._phone = phoneNormalized;
+  }
+}
 
 const ernie = new Pet('dog', 1, 'pug', 'yip yip');
 const vera = new Pet('dog', 8, 'border collie', 'woof woof');
 
-ernie.owner = 'Ashley';
+ernie.owner = new Owner('Ashley', '123 main st');
+ernie.owner.phone = '(111)222-3333';
 
-
-
+console.log(ernie.owner.name);
+console.log(ernie.owner);
 
